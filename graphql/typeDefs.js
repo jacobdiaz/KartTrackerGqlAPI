fc009108cd83793f26fb0fecbbeb2c9b5273b14f
Jacob Diaz <jacobdiaz@Jacobs-MacBook-Pro.local>
2023-02-02 13:00:57 -0600

    [chore] Setting up server env

---
+++ graphql/typeDefs.js
@@ -0,0 +1,2 @@
+// What are the graphQL types we are expecting to have in our Apollo server
+const { gql } = require("apollo-server");
--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 const { ApolloServer } = require("apollo-server");
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+// mongoose.set("strictQuery", false);
+// mongoose
+//   .connect(process.env.KART_API_KEY, { useNewUrlParser: true })
+//   .then(() => server.listen({ port: process.env.PORT || 4000 }))
+//   .then(({ url }) => {
+//     console.log(`Server running at ${url} `);
+//   });
+
+console.log(process.env.KART_API_KEY);
